451926beb6ac1441e86bc6402227775a9df996b1
Vu Huy Hoang <uchihavuhuyhoang@gmail.com>
2023-02-25 22:33:18 +0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {
 "assets/assets/pdf/CV_Vuhuyhoang.pdf": "a7ecc303e66986b9b1d9b23dabc6478c",
 "assets/FontManifest.json": "39df9c41cd501f3ad6ccf732d69a581d",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "6d6dc4642ac98640b1f1f2b9ecb9db41",
+"assets/NOTICES": "e4dd2344b9a2d6b51c025b7486f2d387",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/shaders/ink_sparkle.frag": "e430f2f229b1a9126b9b96896408ca3d",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
@@ -59,9 +59,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1e4e062d46e79a28220c606e097cecb9",
-"/": "1e4e062d46e79a28220c606e097cecb9",
-"main.dart.js": "3e13ce96d50df5591cd41c18ceddf36f",
+"index.html": "45328945d1de63a228ff74edef0a54ff",
+"/": "45328945d1de63a228ff74edef0a54ff",
+"main.dart.js": "1576b5773d774e744f449b6db6f3566b",
 "manifest.json": "0898e1a0ebb4e54fa60499f524b97931",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
